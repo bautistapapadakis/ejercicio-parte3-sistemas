b6d098263ddc299477d5fd4a21ec783eb2df2338
Bautista Papadakis <bautistapapadakis@gmail.com>
2026-04-23 08:11:47 -0300

    subo archivos

--- act simulacion de redes.py
+++ act simulacion de redes.py
@@ -1,10 +1,12 @@
+import time
+
 class Nodo:
     def __init__(self, nombre):# define una funcion,init(cuando creás un objeto de la clase),self(objeto que estás creando),nombre(dato que le pasás al objeto)
         self.nombre = nombre #Guarda el valor del parámetro
         self.conexiones = []  #guarda una lista de conexiones
 
-    def agregar_conexion(self, nodo):
-        self.conexiones.append(nodo)
+    def agregar_conexion(self, nodo):#Define un método llamado agregar_conexion,self(nodo actual),nodo (querés agregar como conexión)
+        self.conexiones.append(nodo)#es una lista donde guardás los nodos conectados,append(agrega un elemento al final de la lista)
 
     def enviar_mensaje(self, mensaje):
         print(f"\n{self.nombre} envía: {mensaje}")
@@ -19,7 +21,7 @@ class Nodo:
 servidor = Nodo("servidor")
 cliente1 = Nodo("cliente 1")
 cliente2 = Nodo("cliente 2")
-cliente3 = Nodo("cliente 3")
+cliente3 = Nodo("cliente 3") 
 
 
 servidor.agregar_conexion(cliente1)
@@ -28,3 +30,18 @@ servidor.agregar_conexion(cliente3)
 
 
 servidor.enviar_mensaje("Hola clientes, este es el servidor")
+
+time.sleep(2)
+print("\nSimulando desconexión y reconexión dinámica...")
+
+servidor.eliminar_conexion(cliente2)
+
+# Enviar mensaje (cliente2 no recibe)
+servidor.enviar_mensaje("Cliente 2 está desconectado")
+
+# Simular reconexión
+time.sleep(2)
+servidor.agregar_conexion(cliente2)
+
+# Mensaje final
+servidor.enviar_mensaje("¡Hola de nuevo a todos!")
